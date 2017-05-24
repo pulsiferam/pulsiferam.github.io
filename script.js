797871c316776aae4d3329ee6035b5f13f89b421
pulsiferam <pulsiferam@vcu.edu>
2017-05-24 17:19:45 -0400

    Changed "Art Gallery"  to "Gallery", adjusted topnav margin top if using mobile

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function adjustCSS(){
 	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
 		//Adjusts the CSS of certain elements if the page is being loaded on a mobile device
-		document.getElementById("topnav").style.marginTop = "0px";
+		document.getElementById("topnav").style.marginTop = "5px";
+		
 	}
 }
