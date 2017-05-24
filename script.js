1c6be347bb75aeac77b678b4a3e4ef0f4b837c79
pulsiferam <pulsiferam@vcu.edu>
2017-05-23 23:18:22 -0400

    Added JS, testing to see if color changes on topnav if user is on a mobile device.

---
+++ script.js
@@ -0,0 +1,6 @@
+function adjustCSS(){
+	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+		//Adjusts the CSS of certain elements if the page is being loaded on a mobile device
+		document.getElementById("p2").style.background-color = "blue";
+	}
+}
