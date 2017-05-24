3543f45a39421b5e324243c3cd80b9566bf43a36
pulsiferam <pulsiferam@vcu.edu>
2017-05-24 00:19:48 -0400

    Added gallery and store to the topnav

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function adjustCSS(){
 	
-	document.getElementById("topnav").style.margin-top = "5vh";
+	document.getElementById("topnav").style.margin-top = "0vh";
 	/*if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
 		//Adjusts the CSS of certain elements if the page is being loaded on a mobile device
 		
