243ee9d9471313eb44816325ecd038fc32b3a33f
pulsiferam <pulsiferam@vcu.edu>
2017-05-23 23:47:41 -0400

    Testing js stuff

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function adjustCSS(){
-	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+	
+	document.getElementById("topnav").style.margin-top = "5vh";
+	/*if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
 		//Adjusts the CSS of certain elements if the page is being loaded on a mobile device
-		document.getElementById("topnav").style.background-color = "blue";
-	}
+		
+		
+	}*/
 }
