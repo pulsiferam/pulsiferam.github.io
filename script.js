71d22f01d554837a5c28345308a36c5871ed82f2
pulsiferam <pulsiferam@vcu.edu>
2017-05-24 14:31:01 -0400

    Added JS to change the page if user is using a mobile device

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 function adjustCSS(){
-	
-	document.getElementById("topnav").style.margin-top = "0vh";
-	/*if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
 		//Adjusts the CSS of certain elements if the page is being loaded on a mobile device
-		
-		
-	}*/
+		document.getElementById("topnav").style.marginTop = "0px";
+	}
 }
