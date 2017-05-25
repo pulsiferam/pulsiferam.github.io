0b296dd4c42a00b5e80340fae6da8121193754b6
pulsiferam <pulsiferam@vcu.edu>
2017-05-25 15:53:40 -0400

    Adjusting JS

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
 function adjustCSS(){
 	var w = window.outerWidth;
     var h = window.outerHeight;
-	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-		//Adjusts the CSS of certain elements if the page is being loaded on a mobile device
-		document.getElementById("topnav").style.marginTop = "5vh";
-		document.getElementById("button1").style.width = "4vw";
-		document.getElementById("button2").style.width = "4vw";
-		document.getElementById("button3").style.width = "4vw";
-		document.getElementById("button4").style.width = "4vw";
-		document.getElementById("button5").style.width = "4vw";
-		document.getElementById("button6").style.width = "4vw";
-		document.getElementById("button7").style.width = "4vw";
-		document.getElementById("button8").style.width = "4vw";
-		document.getElementById("button9").style.width = "4vw";
-	}
 	
 	if(w <= 935){
 		document.getElementById("topnav").style.marginTop = "5vh";
@@ -59,5 +46,18 @@ function adjustCSS(){
 		document.getElementById("button7").style.fontSize = "1vw";
 		document.getElementById("button8").style.fontSize = "1vw";
 		document.getElementById("button9").style.fontSize = "1vw";
+	}
+	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+		//Adjusts the CSS of certain elements if the page is being loaded on a mobile device
+		document.getElementById("topnav").style.marginTop = "5vh";
+		document.getElementById("button1").style.width = "4vw";
+		document.getElementById("button2").style.width = "4vw";
+		document.getElementById("button3").style.width = "4vw";
+		document.getElementById("button4").style.width = "4vw";
+		document.getElementById("button5").style.width = "4vw";
+		document.getElementById("button6").style.width = "4vw";
+		document.getElementById("button7").style.width = "4vw";
+		document.getElementById("button8").style.width = "4vw";
+		document.getElementById("button9").style.width = "4vw";
 	}
 }
