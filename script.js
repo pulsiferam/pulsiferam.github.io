89beb9bc89eecbe5174a7d5171d171087c9eb2f7
pulsiferam <pulsiferam@vcu.edu>
2017-06-08 18:33:35 -0400

    Buttons no longer float left on a mobile device

--- script.js
+++ script.js
@@ -4,16 +4,6 @@ function adjustCSS(){
 	
 	if(w <= 935){
 		document.getElementById("topnav").style.marginTop = "5vh";
-		document.getElementById("button1").style.width = "4vw";
-		document.getElementById("button2").style.width = "4vw";
-		document.getElementById("button3").style.width = "4vw";
-		document.getElementById("button4").style.width = "4vw";
-		document.getElementById("button5").style.width = "4vw";
-		document.getElementById("button6").style.width = "4vw";
-		document.getElementById("button7").style.width = "4vw";
-		document.getElementById("button8").style.width = "4vw";
-		document.getElementById("button9").style.width = "4vw";
-		
 		document.getElementById("button1").style.fontSize = "1.3vw";
 		document.getElementById("button2").style.fontSize = "1.3vw";
 		document.getElementById("button3").style.fontSize = "1.3vw";
@@ -50,6 +40,17 @@ function adjustCSS(){
 	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
 		//Adjusts the CSS of certain elements if the page is being loaded on a mobile device
 		document.getElementById("topnav").style.marginTop = "5vh";
+		
+		document.getElementById("button1").style.float = "none";
+		document.getElementById("button2").style.float = "none";
+		document.getElementById("button3").style.float = "none";
+		document.getElementById("button4").style.float = "none";
+		document.getElementById("button5").style.float = "none";
+		document.getElementById("button6").style.float = "none";
+		document.getElementById("button7").style.float = "none";
+		document.getElementById("button8").style.float = "none";
+		document.getElementById("button9").style.float = "none";
+		
 		document.getElementById("button1").style.width = "4vw";
 		document.getElementById("button2").style.width = "4vw";
 		document.getElementById("button3").style.width = "4vw";
